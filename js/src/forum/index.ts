b117dfcbf031f0a34fae60a10b3ec05f89ab5e47
Lady-Byron <samantha-everett@outlook.com>
2025-11-20 15:48:05 -0500

    Update index.ts

--- js/src/forum/index.ts
+++ js/src/forum/index.ts
@@ -1,24 +1,25 @@
-// js/src/forum/index.ts
 import app from 'flarum/forum/app';
 import Discussion from 'flarum/common/models/Discussion';
 import Model from 'flarum/common/Model';
 
 import installReplyJumpGuard from './features/ReplyJumpGuard';
-import installReadingShortcuts from './features/KeyboardShortcuts'; // 保持你原有的文件内容即可
+import installReadingShortcuts from './features/KeyboardShortcuts';
 import registerViewModifications from './extenders/ViewModifications';
-import registerGlobalNavigation from './extenders/GlobalNavigation'; // 下面会给出一个简化版
+import registerGlobalNavigation from './extenders/GlobalNavigation';
 
 app.initializers.add('lady-byron/reading-enhance', () => {
-  // 注册 Model 属性，让 TS 不报错
+  // 1. 注册 Model 属性 (让前端知道这个属性存在)
   Discussion.prototype.lbReadingPosition = Model.attribute('lbReadingPosition');
 
-  // 安装功能
-  installReplyJumpGuard();
-  installReadingShortcuts(); // 这个文件不需要大改，只要去掉 @ts-ignore 稍微规范下即可
-  
-  // 注册 UI 修改
+  // 2. 注册 UI 修改 (列表页链接 & 阅读页监听)
   registerViewModifications();
-  
-  // 注册全局点击接管 (简化版，去除 mouseover)
+
+  // 3. 注册全局点击拦截 (实现任意链接续读)
   registerGlobalNavigation();
+
+  // 4. 注册发帖防乱跳保护
+  installReplyJumpGuard();
+
+  // 5. 注册快捷键
+  installReadingShortcuts();
 });
